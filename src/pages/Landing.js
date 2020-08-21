d1dc0e5984d71aa1e067dae698fc8ca861db36bc
neirbonhoj <43376907+neirbonhoj@users.noreply.github.com>
2020-08-21 03:26:40 -0500

    adding animation to the login submit button

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,10 @@
 import React from "react";
 import '../css/Landing.css'
 
-const {Component} = require("react");
+import checkmark from '../assets/check.svg'
+import failure from '../assets/failure.svg'
 
+const {Component} = require("react");
 const emailregex = new RegExp("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
 
 
@@ -12,7 +14,10 @@ class Landing extends Component {
 
     this.state = {
       landing_login_email: '',
-      landing_login_password: ''
+      landing_login_password: '',
+      processing_login: false,
+      successful_login: false,
+      failed_login: false
     }
 
     this.onLogin = this.onLogin.bind(this);
@@ -21,6 +26,9 @@ class Landing extends Component {
 
   onLogin(e){
     e.preventDefault();
+    this.setState({
+      processing_login: true
+    })
   }
 
   onInputChange(e){
@@ -30,6 +38,37 @@ class Landing extends Component {
   }
 
   render() {
+    // Animation of the login form submit button
+    //    Arrays are used to store class names dynamically for HTML elements
+    //    processing  = waiting for server response
+    //    success     = server has logged us in
+    //    failed      = credentials are invalid
+    //    hide        = user has not submitted the form
+    let landingLoginSubmitClasses = ['landing-login-submit'];
+    let landingLoginLoadingIconClasses = ['landing-login-loading-icon'];
+    let landingLoginCheckmarkIconClasses = ['landing-login-checkmark-icon'];
+    let landingLoginFailureIconClasses = ['landing-login-failure-icon'];
+    if(this.state.processing_login){
+      landingLoginSubmitClasses.push('processing')
+      landingLoginLoadingIconClasses.push('processing')
+      landingLoginCheckmarkIconClasses.push('hide')
+      landingLoginFailureIconClasses.push('hide')
+    } else if(this.state.successful_login) {
+      landingLoginSubmitClasses.push('processing')
+      landingLoginLoadingIconClasses.push('success')
+      landingLoginCheckmarkIconClasses.push('success')
+      landingLoginFailureIconClasses.push('hide')
+    } else if(this.state.failed_login) {
+      landingLoginSubmitClasses.push('processing')
+      landingLoginLoadingIconClasses.push('failure')
+      landingLoginCheckmarkIconClasses.push('hide')
+      landingLoginFailureIconClasses.push('failure')
+    } else {
+      landingLoginLoadingIconClasses.push('hide')
+      landingLoginCheckmarkIconClasses.push('hide')
+      landingLoginFailureIconClasses.push('hide')
+    }
+
     return (
       <div className='landing-background'>
         <div className='blur'>
@@ -40,7 +79,12 @@ class Landing extends Component {
               <br/>
               <input className='landing-login-input' type='password' name='landing_login_password' onChange={this.onInputChange} placeholder='Password'/>
               <br/>
-              <input className='landing-login-submit' type='submit' value='Sign In'/>
+              <div className='landing-login-submit-container'>
+                <input className={landingLoginSubmitClasses.join(' ')} type='submit' value='Sign In'/>
+                <div className={landingLoginLoadingIconClasses.join(' ')}/>
+                <img className={landingLoginCheckmarkIconClasses.join(' ')} src={checkmark} alt='checkmark'/>
+                <img className={landingLoginFailureIconClasses.join(' ')} src={failure} alt='failure'/>
+              </div>
             </form>
           </div>
         </div>
