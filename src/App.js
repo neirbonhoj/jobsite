07c986ed8cf5be8207db49588f172bf06d9fa14f
neirbonhoj <43376907+neirbonhoj@users.noreply.github.com>
2020-08-22 14:41:59 -0500

    simplify animations and change background photo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { firebaseConfig } from "./firebaseconfig";
 import Landing from './pages/Landing';
-import Contact from "./pages/Contact";
 
 import * as firebase from "firebase/app";
 
@@ -14,9 +13,6 @@ function App() {
         <Switch>
           <Route path='/'>
             <Landing />
-          </Route>
-          <Route path='/contact'>
-            <Contact />
           </Route>
         </Switch>
       </div>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,6 @@
 import React from "react"
 import '../css/Landing.css'
 
-import continueicon from '../assets/continue.svg'
-import loading from '../assets/loading.svg'
-import checkmark from '../assets/check.svg'
-import successring from '../assets/success-ring.svg'
-import failure from '../assets/failure.svg'
-import failurering from '../assets/failure-ring.svg'
-
 const { Component } = require("react")
 const emailregex = new RegExp("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
 
@@ -21,54 +14,74 @@ class Landing extends Component {
     this.state = {
       landing_login_email: '',
       landing_login_password: '',
-      processing_email: false,
-      confirmed_email: false
     }
 
     this.onLogin = this.onLogin.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   onLogin(e){
-    e.preventDefault();
+    if(e){
+      e.preventDefault();
+    }
     this.setState({
-      processing_email: true
+      processing_login: true
     })
-
-    firebase.auth().fetchSignInMethodsForEmail(this.state.landing_login_email).then((signInMethods) => {
-      if(signInMethods.includes('password')){
+    firebase.auth().signInWithEmailAndPassword(this.state.landing_login_email, this.state.landing_login_password)
+      .then((credential) => {
         this.setState({
-          confirmed_email: true
+          processing_login: false,
+          successful_login: true,
+          failed_login: false,
+          failed_password: false,
+          failed_email: false
         })
-      }
+      }, ((rejection) => {
+        if(rejection.code === 'auth/wrong-password'){
+          this.setState({
+            processing_login: false,
+            failed_login: true,
+            failed_password: true,
+            failed_email: false
+          })
+        } else if(rejection.code === 'auth/user-not-found' || rejection.code === 'auth/invalid-email'){
+          this.setState({
+            processing_login: false,
+            failed_login: true,
+            failed_email: true,
+            failed_password: false
+          })
+        }
+      })).catch((error) => {
+        console.log(error)
     })
   }
 
   onInputChange(e){
     this.setState({
+      successful_login: false,
+      failed_login: false,
+      failed_password: false,
+      failed_email: false,
       [e.target.name]: e.target.value
     });
   }
 
-  render() {
-    let emailArrowClasses = ['login-email-arrow']
-    let passwordArrowClasses = ['login-password-arrow']
+  handleKeyDown(e){
+    if (e.key === 'Enter') {
+      this.onLogin()
+    }
+  }
 
+  render() {
+    let containerClasses = ['landing-container']
     let emailInputClasses = ['landing-login-input']
     let passwordInputClasses = ['landing-login-input']
-
-    if(this.state.confirmed_email){
-      emailInputClasses.push('topborderonly');
-      emailArrowClasses.push('hide');
-      passwordInputClasses.push('show');
-      passwordArrowClasses.push('show');
-    }
+    let submitButtonClasses = ['login-submit-button']
 
     // Checks on input values
     let validEmail = true
-    if(this.state.landing_login_password.length < 6) {
-      passwordArrowClasses.push('disabled')
-    }
 
     if(this.state.landing_login_email.length > 0 && !emailregex.test(this.state.landing_login_email)){
       validEmail = false
@@ -76,22 +89,40 @@ class Landing extends Component {
       validEmail = false
     }
 
-    if(!validEmail){
-      emailArrowClasses.push('disabled')
+    let validPassword = true
+
+    if(this.state.landing_login_password.length > 0 && this.state.landing_login_password.length < 6){
+      validPassword = false
+    } else if(this.state.landing_login_password.length <= 0){
+      validPassword = false
+    }
+
+    if(this.state.successful_login){
+      containerClasses.push('success')
+    } else if(this.state.failed_login){
+      if(this.state.failed_email){
+        emailInputClasses.push('failure')
+      } else if(this.state.failed_password){
+        passwordInputClasses.push('failure')
+      }
+      containerClasses.push('failure')
+    } else if(this.state.processing_login){
+      containerClasses.push('processing')
     }
 
     return (
       <div className='landing-background'>
         <div className='blur'>
-          <div className='landing-container'>
-            <h1 className='landing-title'>Larkin Motors</h1>
-            <div className='email-input-container'>
-              <input id='email-input' className={emailInputClasses.join(' ')} type='text' name='landing_login_email' onChange={this.onInputChange} placeholder='Email'/>
-              <img className={emailArrowClasses.join(' ')} onClick={this.onLogin} src={continueicon} alt='arrow'/>
-            </div>
-            <div className='password-input-container'>
-              <input id='password-input' className={passwordInputClasses.join(' ')} type='password' name='landing_login_password' onChange={this.onInputChange} placeholder='Password'/>
-              <img className={passwordArrowClasses.join(' ')} src={continueicon} alt='arrow'/>
+          <div className={containerClasses.join(' ')}>
+            <div className='login-form-container'>
+              <h1 className='landing-title'>Larkin Motors</h1>
+              <div className='email-input-container'>
+                <input id='email-input' className={emailInputClasses.join(' ')} type='text' name='landing_login_email' onChange={this.onInputChange} onKeyDown={this.handleKeyDown} placeholder='Email'/>
+              </div>
+              <div className='password-input-container'>
+                <input id='password-input' className={passwordInputClasses.join(' ')} type='password' name='landing_login_password' onChange={this.onInputChange} onKeyDown={this.handleKeyDown} placeholder='Password'/>
+              </div>
+              <button className={submitButtonClasses.join(' ')} disabled={!validEmail || !validPassword} onClick={this.onLogin}>Submit</button>
             </div>
           </div>
         </div>
