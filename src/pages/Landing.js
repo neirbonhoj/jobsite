e891541f14e1ca9e56a507eebed9b352f56bb439
neirbonhoj <43376907+neirbonhoj@users.noreply.github.com>
2020-08-22 17:11:36 -0500

    starting to implement create account

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,7 @@ import React from "react"
 import '../css/Landing.css'
 
 const { Component } = require("react")
+// eslint-disable-next-line
 const emailregex = new RegExp("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
 
 const firebase = require("firebase/app");
@@ -14,11 +15,13 @@ class Landing extends Component {
     this.state = {
       landing_login_email: '',
       landing_login_password: '',
+      creating_account: false
     }
 
     this.onLogin = this.onLogin.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleCreateAccount = this.handleCreateAccount.bind(this);
   }
 
   onLogin(e){
@@ -55,6 +58,13 @@ class Landing extends Component {
         }
       })).catch((error) => {
         console.log(error)
+    })
+  }
+
+  handleCreateAccount(){
+    let current = this.state.creating_account
+    this.setState({
+      creating_account: !current
     })
   }
 
@@ -110,15 +120,15 @@ class Landing extends Component {
       containerClasses.push('processing')
     }
 
-    let pageStatus = 'Log in to your account'
-
     return (
       <div className='landing-background'>
         <div className='blur'>
           <div className={containerClasses.join(' ')}>
             <div className='login-form-container'>
               <h1 className='landing-title'>Larkin Motors</h1>
-              <h2 className='landing-subtitle'>{pageStatus}</h2>
+              <h2 className='landing-subtitle'>{
+                (this.state.creating_account) ? 'Create an account' : 'Log in to your account'
+              }</h2>
               <div className='email-input-container'>
                 <input id='email-input' className={emailInputClasses.join(' ')} type='text' name='landing_login_email' onChange={this.onInputChange} onKeyDown={this.handleKeyDown} placeholder='Email'/>
               </div>
@@ -127,7 +137,9 @@ class Landing extends Component {
               </div>
               <button className={submitButtonClasses.join(' ')} disabled={!validEmail || !validPassword} onClick={this.onLogin}>Submit</button>
             </div>
-            <h2 className='create-account-link'>Create Account</h2>
+            <h2 className='create-account-link' onClick={this.handleCreateAccount}>{
+              (this.state.creating_account) ? 'Return' : 'Create Account'
+            }</h2>
           </div>
         </div>
       </div>
